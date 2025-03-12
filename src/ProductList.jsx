d383fda2a383f859f7ae1398e8a79ba3600eab02
oort3x <clareshevket@gmail.com>
2025-03-12 19:18:29 +0000

    typo

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,7 +2,7 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { useDispatch, useSelector } from 'react-redux';
-import { addItem } from './CreateSlice';
+import { addItem } from './CartSlice';
 function ProductList(props) {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
